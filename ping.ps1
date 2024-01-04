a816396cae7cecb4e48a3ec1bb6e4c7a6f9b66f3
vMawk <117638856+vMawk@users.noreply.github.com>
2024-01-04 14:23:03 +0100

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,4 +1,4 @@
-        Write-Host "------------------Talpa Network-------------------"
+Write-Host "------------------Talpa Network-------------------"
 $computer = Read-Host -Prompt "Enter the name of the computer you want to check"
 $ping = Test-Connection -ComputerName $computer -Count 1 -ErrorAction SilentlyContinue
 if ($ping) {
@@ -9,10 +9,40 @@ if ($ping) {
         $uptime = (Get-Date) - $uptime
         Write-Host "                                                  "
         Write-Host "The following users are logged in to $($computer):"
-        Write-Host $users
+        Write-Host $users -ForegroundColor yellow
+        Write-Host "The uptime of $($computer) is $($uptime.Days) days, $($uptime.Hours) hours, $($uptime.Minutes) minutes, and $($uptime.Seconds) seconds." -ForegroundColor black -BackgroundColor white
         Write-Host "                                                  "
         Write-Host "                                                  "
-        Write-Host "The uptime of $($computer) is $($uptime.Days) days, $($uptime.Hours) hours, $($uptime.Minutes) minutes, and $($uptime.Seconds) seconds."
+        Write-Host "What would you like to do?"
+        Write-Host "1. Shutdown"
+        Write-Host "2. Restart"
+        Write-Host "3. Sign out user (Not Ready)"
+        Write-Host "4. Exit"
+        $choice = Read-Host -Prompt "Enter your choice (1, 2, 3, or 4)"
+        switch ($choice) {
+            1 {
+                Stop-Computer -ComputerName $computer -Force
+            }
+            2 {
+                Restart-Computer -ComputerName $computer -Force
+            }
+            3 {
+                $session = (Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $computer).SessionId
+                $result = (Invoke-CimMethod -ClassName Win32_OperatingSystem -ComputerName $computer -MethodName Win32Shutdown -Arguments @{Flags = 0;Reserved = 0;ReasonCode = 0;Comment = "";Timeout = 0;Force = $true}).ReturnValue
+                if ($result -eq 0) {
+                    Write-Host "User has been signed out successfully."
+                } else {
+                    Write-Host "Failed to sign out user."
+                }
+            }
+            4 {
+                Write-Host "Exiting the script..."
+                Exit
+            }
+            default {
+                Write-Host "Invalid choice. Please enter 1, 2, 3, or 4."
+            }
+        }
     } else {
         Write-Host "No one is logged in to $($computer)."
     }
